cdd918bb35f89d7c9fe021ed05e194ee789b6928
abdul wassy <mabdulwassaykhan@gmail.com>
2022-12-28 13:56:36 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     <h1>Hello Abd-ul-wassay</h1>
+     <p>My Name Is Muhammad Abd-ul-wassay Khan</p>
+    </div>
+  )
+}
+
+export default App
